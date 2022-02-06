080efcf6c0d2c0d67caac4366b3d7be720cb1496
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-02-06 13:41:54 +0200

    add burger

---
+++ src/js/header.js
@@ -0,0 +1,43 @@
+// ------------ Добавляет класс  другому тегу -----------
+
+$(document).ready(function () {
+    $(".menu__burger--menu-close").click(function () {
+        $(".navigation__menu").toggleClass("navigation__menu--is-active"); return false;
+    });
+});
+
+
+$(document).ready(function () {
+    $(".menu__burger").click(function () {
+        $(".navigation__menu").toggleClass("navigation__menu--is-active");
+         $(".menu__burger--menu-close").toggleClass("menu__burger--menu-is-open"); return false;
+    });
+});
+
+// -------------  Добавляет класс  тому-же тегу
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu]");
+    // const mobileMenuRef = document.querySelector("[data-menu-open]");
+    // const btnReadGreen = document.querySelector("[data-menu-green]");
+  
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            
+            menuBtnRef.classList.toggle("menu__burger--menu-is-open");
+    
+       
+             });
+    //     mobileMenuRef.addEventListener("click", () => {
+    //         const expanded =
+              
+    //             mobileMenuRef.classList.remove("menu__burger--menu-is-open");
+    //     });
+    
+    // btnReadGreen.addEventListener("click", () => {
+    //     const expanded =
+           
+    //         btnReadGreen.classList.toggle("is-close-three");
+    
+        
+    //          });
+    })();
